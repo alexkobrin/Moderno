05859049d7515edcdec97e20d4aeea86f8d5c329
alexkobrin <storyday2012@gmail.com>
2020-07-08 10:52:14 +0200

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,23 @@
 $ (function() {
 
+
+$(".rate-star").rateYo({
+        starWidth: "12px",
+        rating: 5,
+        readOnly: true
+      });
+
 var mixer = mixitup('.products__inner-box');
+
+$('.product-slider__inner').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows:false
+
+  });
+ 
 
     
 });
